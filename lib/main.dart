348852d72417bfac234cab143fa73368e369dc4c
Matthew Lalmansingh <gpjazzplayer@gmail.com>
2018-09-04 20:46:51 -0500

    Change Theme
    - Add Theme property to MyApp to make header white

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,13 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  /*@override
-  Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          //child: const Text('Hello World'),
-          //child: new Text(wordPair.asPascalCase),
-          child: new RandomWords(),
-        ),
-      ),
-    );
-  }*/
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: new RandomWords(),
     );
   }
